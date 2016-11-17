2da00442f7813d97571da4cf3db2fa806d873418
Enda Lee <enda.lee@ittdublin.ie>
2016-11-17 12:38:16 +0000

    1st commit, all pages linked

---
+++ app/controllers/HomeController.java
@@ -0,0 +1,48 @@
+package controllers;
+
+import play.mvc.*;
+
+import views.html.*;
+
+/**
+ * This controller contains an action to handle HTTP requests
+ * to the application's home page.
+ */
+public class HomeController extends Controller {
+
+    /**
+     * An action that renders an HTML page with a welcome message.
+     * The configuration in the <code>routes</code> file means that
+     * this method will be called when the application receives a
+     * <code>GET</code> request with a path of <code>/</code>.
+     */
+    public Result index() {
+        return ok(index.render());
+    }
+    
+    public Result about() {
+        return ok(about.render());
+    }
+    
+    public Result contact() {
+        return ok(contact.render());
+    }
+    
+    public Result account() {
+        return ok(account.render());
+    }
+    
+    public Result desktops() {
+        return ok(desktops.render());
+    }
+    
+    public Result laptops() {
+        return ok(laptops.render());
+    }
+    
+    public Result televisions() {
+        return ok(televisions.render());
+    }
+    
+
+}
